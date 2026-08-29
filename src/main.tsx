ec39b5960d02bfa4d9306e5753e17203c88555a4
sonib-67 <gamingbuddyzone@gmail.com>
2026-08-29 13:37:36 +0530

    chore: update sitemap timestamps and env config
    Update sitemap lastmod dates and refactor Google ReCaptcha provider to use environment variables for the site key.

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const container = document.getElementById('root')!;
 const app = (
   <StrictMode>
     <HelmetProvider>
-      <GoogleReCaptchaProvider reCaptchaKey="6LecX54tAAAAAHIaLEDMvKB1ldc6DhCdhlqUz7L5">
+      <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY || "6LecX54tAAAAAHIaLEDMvKB1ldc6DhCdhlqUz7L5"}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
